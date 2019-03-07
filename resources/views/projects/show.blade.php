6e7641a3c193d3db59fe5478ae4b1397a9288798
simshard <30438951+simshard@users.noreply.github.com>
2019-03-07 19:13:33 +0000

    lesson 19

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -59,6 +59,7 @@
 
                         <button type="submit" class="button">Save</button>
                     </form>
+                     @include ('errors')
                 </div>
             </div>
 
